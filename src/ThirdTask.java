987467a5eb9d4ed3e60f91961f31fd493464fc81
andriusbaltrunas <andriusbaltrunas@yahoo.com>
2018-03-21 20:21:59 +0200

    do while su blogu skaiciumi

---
+++ src/ThirdTask.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * Created by andriusbaltrunas on 3/21/2018.
+ */
+public class ThirdTask {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int sk = 0;
+        int kiek = 0;
+        int[] mas = {};
+
+        do {
+            System.out.println("Veskite tol kol bus ivestas ne 0");
+            try {
+                sk = scanner.nextInt();
+                if (mas.length <= kiek) {
+                    mas = Arrays.copyOf(mas, mas.length + 1);
+                }
+                mas[kiek++] = sk;
+            } catch (InputMismatchException e) {
+                System.out.println("Blogai ivestas skaicius, programa baigia darba");
+                scanner.nextLine();
+            }
+        } while (sk != 0);
+
+        System.out.println("Suma yra :" + masSum(mas));
+
+    }
+
+    private static int masSum(int[] mas) {
+        int sum = 0;
+        for (Integer m : mas) {
+            sum += m;
+        }
+        return sum;
+    }
+}
